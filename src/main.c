d74de1bfc8bcb98f785483e1e3337d320bf2d69e
Reuben Thomas <rrt@sc3d.org>
2022-01-21 15:12:35 +0000

    Bump version to 3.7.10 & add NEWS; bump copyright year to 2022

--- src/main.c
+++ src/main.c
@@ -628,7 +628,7 @@ Written by François Pinard <pinard@iro.umontreal.ca>.\n"),
 	     stdout);
       fputs (_("\
 \n\
-Copyright (C) 1990-2021 Free Software Foundation, Inc.\n"),
+Copyright (C) 1990-2022 Free Software Foundation, Inc.\n"),
 	     stdout);
       fputs (_("\
 This is free software; see the source for copying conditions.  There is NO\n\
