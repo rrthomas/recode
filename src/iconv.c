rrthomas/recode

commit 162f24fd93b7b48fa113b949d684623854213a59

iconv: let iconv handle invalid/untranslatable input
Rather than try to skip such input in recode, allow iconv to skip it or not,
according to //IGNORE and //TRANSLIT settings.

src/iconv.c
@@ -134,16 +134,8 @@ wrapped_transform (iconv_t conversion, RECODE_SUBTASK subtask)
                   iconv_close (check_conversion);
 		}
 
-	      /* Invalid or untranslatable input.  Skip one byte.  */
-              /* FIXME: We cannot tell how many bytes to skip for
-                 untranslatable input.  The likely result is that we'll
-                 get an "invalid input" error on the next step. */
+	      /* Invalid or untranslatable input.  */
 	      RETURN_IF_NOGO (recode_error, subtask);
-	      assert (input_left > 0);
-	      input++;
-	      input_left--;
-	      /* Why is draining required?  */
-	      drain_first = true;
 	    }
 	  else if (saved_errno == EINVAL)
 	    {

commit 1ca6b4bba5355444e328ee54da88d3fed2cd4308

iconv: fix an infinite loop on invalid/untranslatable input
This bug was introduced in commit 162f24f.

If iconv gives an error, and we do not return with an error ourselves, then
skip a byte (we can’t tell how much we should skip!).

src/iconv.c
@@ -149,6 +149,13 @@ wrapped_transform (iconv_t conversion, RECODE_SUBTASK subtask)
 
 	      /* Invalid or untranslatable input.  */
 	      RETURN_IF_NOGO (recode_error, subtask);
+	      /* Ensure we skip at least one byte.
+		 FIXME: We cannot tell how many bytes to skip for
+		 untranslatable input.  The likely result is that we'll
+		 get an "invalid input" error on the next step. */
+	      assert (input_left > 0);
+	      input++;
+	      input_left--;
 	    }
 	  else if (saved_errno == EINVAL)
 	    {